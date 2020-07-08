e0646150a5475b04b4d542b18bc8e0317103edf0
Olivier Gutierrez <olivier.gutierrez@limayrac.fr>
2020-07-08 19:56:41 +0000

    c2 rajoutE url pour forwarder immEdiatement vers lycee au demarrage (avec url vide)

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,10 @@ from django.conf.urls import include,url
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
+    # url pour l'administration
+    path('admin/', admin.site.urls),
+    # url pour forwarder immEdiatement vers lycee au demarrage (avec url vide)
+    url(r'^', include('lycee.urls')), 
     url(r'^lycee/', include('lycee.urls')),
     #path(r'^lycee/' ,  lycee.urls),
     #url(r'^lycee/', include('lycee.urls', "lycee","lycee")),
